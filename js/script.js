e271943328a0fefa00c7c8f0b93bcfc072bcb256
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-25 18:19:47 +0000

    html + js finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // variable, user input
+  const userAge = parseInt(document.getElementById("user-age").value)
+
+  if (userAge >= 17) {
+    // output
+    document.getElementById("movie-type").innerHTML = "You can see an R rated movie alone."
+  } else if (userAge >= 13) {
+    // output
+    document.getElementById("movie-type").innerHTML = "You can see a PG-13 movie alone."
+  } else if (userAge >= 5) {
+    // output
+    document.getElementById("movie-type").innerHTML = "You can see a G or PG movie alone."
+  } else {
+    // output
+    document.getElementById("movie-type").innerHTML = "Uh. You're too young for most things."
+  }
+  // output
+  document.getElementById("end-sentence").innerHTML = "Thanks for verifying your age!"
+}
